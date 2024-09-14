b8bbb9c6a91ed24075bd7cc059e7dad098067731
Kevin <550031@student.saxion.nl>
2024-09-14 14:06:12 +0200

    Removed old game files

--- Assets/FPSAssets/Scripts/ISoundEmitting.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface ISoundEmitting
-{
-    public void PlayObjectSpecificSound();
-}
--- Assets/FPSAssets/Scripts/ScriptableObjectScripts/WeaponScriptableObject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "WeaponScriptableObject", menuName = "ScriptableObjects/WeaponScriptableObject", order = 1)]
-public class WeaponScriptableObject : ScriptableObject
-{
-    public float fireRate { get; private set; }
-}
