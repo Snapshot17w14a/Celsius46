4bbac60bf01eca51bb5cbef54abc03b946a73885
Aleksandar Lyubenov <aleksandr.lyubenov@gmail.com>
2024-09-26 12:46:13 +0200

    Version 1.0.0.1
    Fixed space not switching modes.

--- Assets/Scripts/ModeSwitch.cs
+++ Assets/Scripts/ModeSwitch.cs
@@ -22,7 +22,10 @@ public class ModeSwitch : MonoBehaviour
 
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            SwitchMode();
+        }
     }
 
     public void SwitchMode()
